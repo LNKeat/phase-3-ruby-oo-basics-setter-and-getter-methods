4daed82177fce77b73e6916f871f65a6cb7e10cc
Laura <lnkeat@gmail.com>
2021-11-17 18:38:50 -0700

    commit tree 20220bcc8164c59096464fd9a97fa7bf2efa56e6

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
+require 'pry'
 class Dog
 
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  # attr_accessor :name
+  # # attr_accessor :breed
+  # def initialize(name, breed)
+  #   @name = name
+  #   @breed = breed
+  # end
 end
+
